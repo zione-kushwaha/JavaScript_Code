a8583c44c29271398103e89ca345efb14529b742
zione-kushwaha <jivankushwaha461@gmail.com>
2023-11-16 19:20:14 +0545

    5th commit

---
+++ operation.js
@@ -0,0 +1,28 @@
+let value= 3
+console.log(-value)  // -3
+
+// Basic arthmetic operation
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(5/2) //2.5
+console.log(5%2)
+
+// string operation
+
+console.log('JKk'+ ' Akk') // Jkk Akk
+console.log('2'+1)//21 
+console.log('2'+1+3)//213
+console.log(1+2+'5') //35
+// conclusion is that more preriority is given to whom which come first
+
+console.log(+true) //1 ----# note true+-> gives error
+
+// prefix and postfix 
+ let x=2
+// console.log(2++)            // this gives error messages
+// console.log(++2)
+
+console.log(x++)    // post increment
+console.log(++x)    // pre increment
+
