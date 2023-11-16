93542361d46cecc474ec6b0fd60341220f01917b
zione-kushwaha <jivankushwaha461@gmail.com>
2023-11-16 13:47:41 +0545

    second commit

---
+++ variable_const.js
@@ -0,0 +1,16 @@
+const accountId=14435
+let email='zionekushwaha@google.com'
+let password=1234
+accountCity='kalaiya'
+
+// accountId=2343
+// console.log(accountId)---------------> this gives error since const can't be modified
+ 
+/*using at the end ";" is optional 
+we should write the code in clear and orginised nature 
+*/
+email='jivankushwaha@google.com'// this is accepted since this email is variable
+
+//similarly for the accountCity ----using let is optional but it is recommended
+
+console.table([email,accountId,password,accountCity])
