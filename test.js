988f36a2b304b799ef0ae8d35f2066cfadec9842
zione-kushwaha <jivankushwaha461@gmail.com>
2023-11-16 13:39:11 +0545

    initial commit

--- test.js
+++ test.js
@@ -2,3 +2,6 @@
 
 //program to print any thing in the console
 console.log('Hellow World of the JavaScript programming');
+
+//printing data in the form of the table 
+console.table(['ram','shyam','jivan'])
