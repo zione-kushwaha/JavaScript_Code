dc93e9b925e9fe9bf5a0d6af27fb3012c51e4c8a
zione-kushwaha <jivankushwaha461@gmail.com>
2023-11-16 16:34:28 +0545

    new commit

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+"use strict"// treat all the JS code as newer version
+
+// alert(3+3)  ------> we are using the node js ,not browser
+
+console.log(3+
+    3)    // this is also works fine but code readability should be high 
+
+    console.log('jeevan');   //here the use of the ";" is optional we can but 
+                            // it is generally not used 
+
+    let names='jeevan'
+    let age=20
+    let RelationshipStatus =false
+    
+    console.table([typeof names,typeof age,typeof RelationshipStatus])
+
+    //new things to be remember null type is the type of the object 
+    // but the undefine type is undefine 
+
+    console.log(typeof null)
+    console.log(typeof undefined)
--- variable_const.js
+++ variable_const.js
@@ -1,6 +1,7 @@
 const accountId=14435
 let email='zionekushwaha@google.com'
 let password=1234
+var name='jivan'
 accountCity='kalaiya'
 
 // accountId=2343
@@ -14,3 +15,5 @@ email='jivankushwaha@google.com'// this is accepted since this email is variable
 //similarly for the accountCity ----using let is optional but it is recommended
 
 console.table([email,accountId,password,accountCity])
+
+/*prefer not to use var because of issue in block scope and functional scope */
