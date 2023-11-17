fc826036fb426e4ee1537b0444952cc9cdb62630
zione-kushwaha <jivankushwaha461@gmail.com>
2023-11-17 08:18:22 +0545

    6 th commit

---
+++ 6_comparision.js
@@ -0,0 +1,13 @@
+console.log(2<1)  //false
+
+console.log('2'>1)   //true
+console.log('02'>1)//true
+
+console.log(null>0)//false
+console.log(null==0)//false
+console.log(null>=0)// true
+// prin
+console.log(undefined>0)
+console.log(undefined==0)   // in all three cases false result
+console.log(undefined>=0)
+
