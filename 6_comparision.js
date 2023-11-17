ad8e6914eb66758f0081930391acf27d9d6e3878
zione-kushwaha <jivankushwaha461@gmail.com>
2023-11-17 08:22:48 +0545

    6th commit

--- 6_comparision.js
+++ 6_comparision.js
@@ -6,8 +6,11 @@ console.log('02'>1)//true
 console.log(null>0)//false
 console.log(null==0)//false
 console.log(null>=0)// true
-// prin
+// comparision converts  the null to an number , treated it as 0
+
 console.log(undefined>0)
 console.log(undefined==0)   // in all three cases false result
 console.log(undefined>=0)
+
+console.log(typeof 34567n)
 
